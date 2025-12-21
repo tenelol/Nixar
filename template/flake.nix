d732ba9897f535ea5148fec0d13217823663d009
tenelol <matsuda319@icloud.com>
2025-12-22 02:35:47 +0900

    Fix template Go module versions

--- template/flake.nix
+++ template/flake.nix
@@ -3,14 +3,12 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
-    nixar.url = "github:tenelol/Nixar";
   };
 
-  outputs = { self, nixpkgs, nixar }:
+  outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
-      nixarPkg = nixar.packages.${system}.server;
     in {
       packages.${system}.default = pkgs.buildGoModule {
         pname = "nixar-template";
@@ -20,9 +18,6 @@
         modRoot = ".";
         subPackages = [ "./cmd/server" ];
         vendorHash = null;
-        postPatch = ''
-          go mod edit -replace github.com/tenelol/nixar=${nixar}
-        '';
       };
 
       apps.${system}.default = {
--- template/go.mod
+++ template/go.mod
@@ -2,7 +2,4 @@ module nixar-template
 
 go 1.22
 
-require github.com/tenelol/nixar v0.0.0
-
-replace github.com/tenelol/nixar => ../
-
+require github.com/tenelol/nixar v0.0.0-20251221173058-f12569944a44
